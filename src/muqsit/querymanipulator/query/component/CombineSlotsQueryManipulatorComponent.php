cf31693032baeb563bb09276ce66564309e2f02f
Muqsit <hackhack05@gmail.com>
2021-02-13 09:50:51 +0000

    Get rid of Ds dependency

--- src/muqsit/querymanipulator/query/component/CombineSlotsQueryManipulatorComponent.php
+++ src/muqsit/querymanipulator/query/component/CombineSlotsQueryManipulatorComponent.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace muqsit\querymanipulator\query\component;
 
-use Ds\Set;
 use muqsit\querymanipulator\server\ServerQueryInfo;
 use pocketmine\network\query\QueryInfo;
 
@@ -14,11 +13,11 @@ final class CombineSlotsQueryManipulatorComponent implements QueryManipulatorCom
 		return new self($configuration["servers"], $configuration["max_slots"]);
 	}
 
-	/** @var Set<string> */
-	private $server_identifiers;
-
 	/** @var bool */
 	private $manipulate_max;
+
+	/** @var string[] */
+	private $server_identifiers = [];
 
 	/** @var int[] */
 	private $players = [];
@@ -31,12 +30,14 @@ final class CombineSlotsQueryManipulatorComponent implements QueryManipulatorCom
 	 * @param bool $manipulate_max
 	 */
 	public function __construct(array $server_identifiers, bool $manipulate_max){
-		$this->server_identifiers = new Set($server_identifiers);
 		$this->manipulate_max = $manipulate_max;
+		foreach($server_identifiers as $server_identifier){
+			$this->server_identifiers[$server_identifier] = $server_identifier;
+		}
 	}
 
 	public function onReceiveUpdate(string $identifier, ServerQueryInfo $info) : void{
-		if($this->server_identifiers->contains($identifier)){
+		if(isset($this->server_identifiers[$identifier])){
 			$this->players[$identifier] = $info->players;
 			$this->max_players[$identifier] = $info->max_players;
 		}
